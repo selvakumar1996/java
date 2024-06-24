8c98324cdd1ddf6618c1aaad97cf5ec3c30a1140
Selvakumar <ssk14121996@gmail.com>
2024-06-24 22:13:38 +0530

    projects

---
+++ src/JavaMethods.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class JavaMethods {
+    public static void main(String[] args) {
+        //initializing number to compare
+        int a = 25, b = 37, c = 29;
+        //comparing numbers a with b anb b with c
+        //if both conditions are true print a
+        if (a <= b && a <= c) ;
+
+        System.out.println(a + "smallest number");
+        //comparing b with a and b with c
+        //if both conditions are true,prints b
+        if (b <= a && b <= c) ;
+        System.out.println(b + "is the smallest number");
+        //print c if the above conditions are false
+        System.out.println(c + "is the smallest number");
+
+
+
+
+
+
+        }
+
+
+    }
